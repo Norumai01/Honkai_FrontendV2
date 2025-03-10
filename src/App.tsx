a8dee12c7e2a87ad300a509f1f0d949560aebcad
Norumai01 <johnnydinhnguyen2001@gmail.com>
2025-03-09 22:18:45 -0400

    Added Register page, needs testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import Login from "./pages/Login.tsx";
 import Home from "./pages/Home.tsx";
+import Register from "./pages/Register.tsx";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </Router>
       </div>
---
+++ src/pages/Register.tsx
@@ -0,0 +1,129 @@
+import React, {ChangeEvent, FormEvent, useState} from "react";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import { API_URL } from "../services/api.ts";
+import axios from "axios";
+
+interface RegisterProps {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export default function Register(): React.ReactElement {
+  const [formData, setFormData] = useState<RegisterProps>({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const navigate: NavigateFunction = useNavigate();
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (formData.username === "" || formData.username === null) {
+      setError("Username is required");
+      return
+    }
+
+    if (formData.email === "" || formData.email === null) {
+      setError("Email is required");
+      return
+    }
+
+    if (formData.password === "" || formData.password === null) {
+      setError("Password is not valid");
+      return
+    }
+
+    if (formData.confirmPassword !== formData.password) {
+      setError("Passwords do not match")
+      return
+    }
+
+    const formDataObj = new FormData()
+    formDataObj.append("username", formData.username)
+    formDataObj.append("email", formData.email)
+    formDataObj.append("password", formData.password)
+
+    axios
+      .post(`${API_URL}/auth/createUser`, formDataObj)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((error: Error) => {
+        console.log("Registration failed: ", error);
+        alert("Registeration failed. Please try again later.");
+      })
+  }
+
+  return (
+    <div className="min-h-screen bg-black flex justify-center items-center">
+      <div className="w-full max-w-lg mx-auto">
+        <form className="flex flex-col space-y-6" onSubmit={handleSubmit}>
+          <label className={`text-white text-3xl font-bold text-center ${error ? "mb-10" : "mb-20"}`}>
+            Create an Account
+          </label>
+          {error !== null && (
+            <p className="text-red-400 text-sm font-semibold mb-10">
+              {error}
+            </p>
+          )}
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            placeholder="Username"
+            required
+            className="block w-full bg-white text-black placeholder-gray-300 text-base"
+          />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Username"
+            required
+            className="block w-full bg-white text-black placeholder-gray-300 text-base"
+          />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Password"
+            required
+            className="block w-full bg-white text-black placeholder-gray-300 text-base"
+          />
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            placeholder="Confirm Password"
+            required
+            className="block w-full bg-white text-black placeholder-gray-300 text-base"
+          />
+          <button
+            type="submit"
+            className="p-3 bg-green-600 rounded-2xl text-white cursor-pointer hover:bg-green-400 focus:outline-none"
+          >
+            Sign-Up
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
