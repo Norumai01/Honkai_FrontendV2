0fbf25d0e5257f04322540201387bd1afe369ca8
Norumai01 <johnnydinhnguyen2001@gmail.com>
2025-03-28 18:11:34 -0400

    Added extra information

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,6 +7,7 @@ export const getAuthHeader = (token: string | null) => {
   return token ? { Authorization: `Bearer ${token}` } : {}
 }
 
+// Documentation: https://blog.logrocket.com/understanding-axios-create/
 export const createAPI: AxiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true // Send and receive cookies
